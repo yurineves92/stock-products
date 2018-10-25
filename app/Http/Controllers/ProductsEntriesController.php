6d8c10957b50966f8948e5253f2ac6bf9e5f28fb
Yuri Neves <yurineves92@gmail.com>
2018-10-25 10:31:58 -0200

    entrada de produtos

--- app/Http/Controllers/ProductsEntriesController.php
+++ app/Http/Controllers/ProductsEntriesController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\ProductsEntries;
+use App\Suppliers;
+use App\Products;
+use Request;
+use Session;
 
 class ProductsEntriesController extends Controller
 {
-    //
+    public function index(){
+    	$products_entries = ProductsEntries::all();
+    	return view('products_entries.index')
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+    	
+    }
+
+    public function edit(){
+    	
+    }
+
+    public function update(){
+    	
+    }
+
+    public function destroy(){
+    	
+    }
 }
